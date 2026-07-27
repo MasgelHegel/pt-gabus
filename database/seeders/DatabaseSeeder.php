0574f1fe40b9c86521e61343c437cf85b563bf4c
MASGEL <hegelmantori12@gmail.com>
2026-07-27 16:44:37 +0700

    Add GasProductSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             AdminUserSeeder::class,
             ChartOfAccountsSeeder::class,
             MasterDataSeeder::class,
+            GasProductSeeder::class,
         ]);
     }
 }
